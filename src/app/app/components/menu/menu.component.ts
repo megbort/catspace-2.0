0b69124b247e772268a7e9c54dc66ea4ebfc79a5
Megan Krenbrink <megan.krenbrink@gmail.com>
2024-09-15 11:59:43 -0700

    Design system setup, menu & hero content

--- src/app/app/components/menu/menu.component.ts
+++ src/app/app/components/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
-}
+export class MenuComponent {}
--- src/app/app/pages/home/home.component.ts
+++ src/app/app/pages/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
